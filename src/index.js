d2fc65fea0a5d8a20e8fde36e56fc4e66bd3a03a
Monicah-Ngari <wakarimangari@gmail.com>
2023-12-31 07:24:29 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
-function myCitty(event) {
+function handleSubmit(event) {
   event.preventDefault();
-  let searchInput = document.querySelector(".search-form-input");
-  let cityElement = document.querySelector(".weather-city");
+  let searchInput = document.querySelector("#search-form-input");
+  let cityElement = document.querySelector("#weather-city");
   cityElement.innerHTML = searchInput.value;
 }
 
-let searchForm = document.querySelector(".search-form");
-searchForm.addEventListener("submit", myCitty);
+let searchForm = document.querySelector("#search-form");
+searchForm.addEventListener("submit", handleSubmit);
