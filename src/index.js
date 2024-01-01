6947b521f6ce63528cda0d4dc041fdd61ba011ae
Monicah-Ngari <wakarimangari@gmail.com>
2024-01-01 19:36:46 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
-function currentWeather(response) {
+function refreshWeather(response) {
   let cityTempElement = "#current-temp";
   let myTemp = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
   cityElement.innerHTML = response.data.city;
-
   cityTempElement.innerHTML = Math.round(myTemp);
 }
 
 function searchCity(city) {
   let apiKey = "1a2a473db97faf41f0088oe8t98271ff";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(currentWeather);
+  axios.get(apiUrl).then(refreshWeather);
 }
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
