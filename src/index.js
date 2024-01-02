5cbcf853c0b4d948f7563ee25a76a275bde9564b
Monicah-Ngari <wakarimangari@gmail.com>
2024-01-02 18:33:35 +0300

    commit

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ function refreshWeather(response) {
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
 
-  console.log(cityElement);
+  console.log(response.data);
   cityElement.innerHTML = response.data.city;
   cityTempElement.innerHTML = Math.round(temperature);
 }
